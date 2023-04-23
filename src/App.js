62603f09c027bf732bb04c94b6ce1f05114e591e
jssmith79 <i.am.hef@gmail.com>
2023-04-23 13:10:37 -0700

    finished project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import './App.css'
 
 
 function App() {
+
+  const [githubName, setgithubName] = useState('');
+  const [githubUrl, setgithubUrl] = useState('');
+
+  useEffect(() => {
+    fetch('http://api.github.com/users/learningToCode1234')
+    .then(res => res.json())
+    .then(data => {
+      setgithubName(data.name);
+      setgithubUrl(data.html_url);
+    })
+  }, []);
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{githubName}</h2>
+      <a href={ githubUrl }>Link to Github Profile</a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,27 @@
 
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
 
+describe('when interacting with the github API', () => {
+    beforeEach(() => {
+        fetch.resetMocks();
+    });
+
+    it('receives github name from the rest API', async() => {
+        fetch.mockResponseOnce(JSON.stringify({ name: 'coder'}));
+        render(<App/>);
+        const githubName =  await waitFor(() => {
+            return screen.getByRole('heading', {level: 2 })
+        })
+        expect(githubName).toHaveTextContent('coder');
+    });
+
+
+    test('it receives the github URL for user', async () => {
+        fetch.mockResponseOnce(JSON.stringify({  html_url: 'http;//github.com/learningToCode1234' }));
+        render(<App/>);
+        const githubUrl = await waitFor(() => screen.getByRole('link'));
+        expect(githubUrl).toHaveAttribute('href', expect.stringContaining('github.com'))
+    })
+});
 
